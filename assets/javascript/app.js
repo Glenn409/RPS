e312f258965ec1e54ad5e06c44d5c675381f0cc6
Christopher Glenn <Chrisglenn519@yahoo.com>
2019-10-21 01:29:24 -0500

    added move decision

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,18 +14,26 @@ var firebaseConfig = {
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
-var userRef = database.ref('users');
+var userRef = database.ref('users'); 
+var player1 = database.ref('users/player1');
+var player2 = database.ref('users/player2');
+
 var currentUserID;
-
-//
+var move = '';
+var msg = '';
+var name = '';
+var state = '';
+var start_game = false;
+// var currentUserInfo = database.ref(`/users/${currentUserID}`);
 function getPlayerID(){
-    userRef.once('value',snap =>{
-        var num = snap.numChildren();
-        currentUserID = Object.keys(snap.val())[num - 1];
-        console.log('player created with the ID of : ' + currentUserID);
-        })
-    }
-
+    userRef.once('value', snap =>{
+        if(snap.val().player1.active === false){
+            currentUserID = 'player1';
+        } else if (snap.val().player2.active === false){
+            currentUserID = 'player2';
+        }
+    })
+}
  //removes start and transitions into entering name
 function enterName(){
     $('.enterGame').empty();
@@ -35,24 +43,73 @@ function enterName(){
          <button class='submit'>Start</button>`
     )
 }
-//function to create a player;
-function createPlayer(name,msg,move,state){
-    player = {
-        // ID: id,
-        name: name,
-        msg: msg,
+//function to create/update a player;
+function updatePlayer(player){
+    console.log(player);
+    playerObj = {
+        active: true,
+        id: currentUserID,
         move: move,
+        msg: msg,
+        name: name,
         state: state
     }
-    return player;
+    console.log(playerObj);
+    var updates = {}   
+    if(player === 'player1'){
+        updates['/users/player1'] = playerObj;
+        return database.ref().update(updates);
+    } else if (player === 'player2'){
+        updates['/users/player2'] = playerObj;
+        return database.ref().update(updates);
+    } else {
+        console.log('error in updatePlayer function');
+    }
+
 }
-//checks if lobby is full, aka if database holds 2 objects alrdy.
+
+//resets player
+function resetPlayers(player){
+    console.log(player);
+    playerObj1 = {
+        active: false,
+        id: 'player1',
+        move: "",
+        msg: "",
+        name: "",
+        state: ""
+    }
+    playerObj2 = {
+        active: false,
+        id: 'player2',
+        move: "",
+        msg: "",
+        name: "",
+        state: ""
+    }
+    var updates = {}   
+    if(player === 'player1'){
+        updates['/users/player1'] = playerObj1;
+        return database.ref().update(updates);
+    } else if (player === 'player2'){
+        updates['/users/player2'] = playerObj2;
+        return database.ref().update(updates);
+    } else {
+
+    }
+
+}
+//checks if lobby is full
 function isLobbyFull(){
     var returnValue;
-    userRef.once('value', snap => {
-       if(snap.numChildren() >= 2){
-           returnValue = true;
-       } else returnValue = false;
+    userRef.once('value', snap =>{
+        if(snap.val().player1.active === true && snap.val().player2.active === true){
+            returnValue = true;
+        } else if (snap.val().player1.active === false){
+                returnValue = 'player1';
+        } else if (snap.val().player2.active === false){
+            returnValue = 'player2';
+        }
     })
     return returnValue;
 }
@@ -62,30 +119,34 @@ function gameIsFull(name){
                 <button class='tryAgain'>Try Again</button>`
     $('.enterGame').html(full)
 }
-function gameScene(){
-
-}
 
 //directs user to gamepage
 function gameDisplay(){
-    var displayBoard = (`<div class=timer>timer Here></div>
+    var displayBoard = (`<div class=timer>timer Here></ 1div>
                         <div class='board'>
-                            <div class='player1'>player1 here</div>
-                            <div calss='player2'>player2 here</div>
+                            <div class='player1'>Player 2</div>
+                            <div class='vs'>VS</div>
+                            <div calss='player2'>Player 2</div>
                         </div>`);
 
     var playerSelection = (`<div class='playerSelection'>
-                            <button class='choice'><img href = ""/>rock</button>
-                            <button class='choice'><img href = ""/>paper</button>
-                            <button class='choice'><img href = ""/>scissors</button>
-                            </div>`)
+                            </div>
+                            <div>${name} you are ${currentUserID}.</div>`)
+
+    var rdyButton = (`<button class='rdy'>Ready Up!</button>`);
     $('.enterGame').empty();
-    $('.enterGame').append(displayBoard, playerSelection);
+    $('.enterGame').append(displayBoard, playerSelection, rdyButton);
 }
 //will run functions to run game
 //1. check if both players are ready
 //2. when both players are ready will start player selection & timer
 function startGame(){
+    $('.rdy').remove();
+    var buttons = `<button class='choice'><img href = ""/>rock</button>
+                   <button class='choice'><img href = ""/>paper</button>
+                   <button class='choice'><img href = ""/>scissors</button>
+                   <div class='youChose'></div>`
+    $('.playerSelection').append(buttons);
 }
 
 
@@ -97,23 +158,54 @@ $('.start').on('click',function(){
 
 //submits user into database if not full
 $(document).on('click','.submit',function(){
-    var userName = $('#nameInput').val().trim();
+    name = $('#nameInput').val().trim();
+    var inactiveSpot = isLobbyFull();
 
-    if(isLobbyFull()){
+    if(inactiveSpot === true){
         console.log('lobby is full');
-        gameIsFull(userName);
+        gameIsFull(name);
     } else {
-        var user = createPlayer(userName,'','none','notready');
-        userRef.push(user);
+        console.log('empty seat at '+ inactiveSpot);
         getPlayerID();
+        updatePlayer(inactiveSpot);
         gameDisplay();
-        startGame();
     }
 })
 //try again button when lobby is currently full
 $(document).on('click','.tryAgain',function(){
     enterName();
 })
+
+//changes players state to rdy if both r rdy games starts
+$(document).on('click','.rdy', function(){
+    state = 'ready';
+    $('.rdy').text('You are Ready!');
+    updatePlayer(currentUserID);
+    userRef.on('value',snap =>{
+        if (snap.val().player1.state === 'ready' && snap.val().player2.state === 'ready'){
+            if(start_game === false){
+                startGame();
+                start_game = true;
+            }
+        }
+    })
+
+})
+
+//sets your current move
+$(document).on('click','.choice',function(){
+    move = $(this).text();
+    updatePlayer(currentUserID);
+    $('.youChose').text('You Chose '+ move);
+})
+
+//resets database
+$('.reset').on('click',function(){
+    resetPlayers('player1');
+    resetPlayers('player2');
+})
+
+
 
 userRef.on('value', snap => {
 
@@ -128,5 +220,5 @@ userRef.on('value', snap => {
 // })
 database.ref().on('value',snap =>{
 
-   
+
 })
